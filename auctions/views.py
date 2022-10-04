7f3e57f1a9c8a76cf4cc6de56be8ff841080fe1d
unknown <onghoangcodebug01@gmail.com>
2022-10-04 18:05:51 +0700

    done specification 1

--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,23 @@
+import time
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.forms import Textarea
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from .models import *
-
+from django import forms
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html",{
+        "list":auction_item.objects.all(),
+    })
+
+class NewItem(forms.Form):
+    name= forms.CharField()
+    st_bid=forms.FloatField(label="Starting bid")
+    description= forms.CharField(widget=Textarea)
 
 
 def login_view(request):
@@ -61,3 +70,19 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def new_item(request):
+    if request.method == "POST"  :
+        f= NewItem(request.POST)
+        if f.is_valid():
+            name=f.cleaned_data["name"]
+            st_bid=f.cleaned_data["st_bid"]
+            description=f.cleaned_data["description"]
+            auction_item.objects.create(name=name, cur_bid=st_bid,description=description,created_by=request.user)
+            return render(request, "auctions/index.html",{
+                "list":auction_item.objects.all(),
+            })
+    return render(request,"auctions/new.html",{
+        "form": NewItem()
+    })
+
